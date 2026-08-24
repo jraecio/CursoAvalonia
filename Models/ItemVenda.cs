1a316126eeb53f1fe09eff2002e4bca716a55480
Aécio Gomes Da Costa junior <a.jrcosta@LIVE.COM>
2026-08-24 20:55:11 -0300

    Primeiro codigo

---
+++ Models/ItemVenda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoAvalonia.Models
+{
+    public class ItemVenda
+    {
+        public int Codigo { get; set; }
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public decimal Quantidade { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public decimal Total
+        {
+            get
+            {
+                return Quantidade * Valor;
+            }
+        }
+    }
+}
---
+++ Models/Venda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CursoAvalonia.Models
+{
+    public class Venda
+    {
+        public List<ItemVenda> Itens { get; set; } = new List<ItemVenda>();
+
+        public decimal Desconto { get; set; }
+
+        public decimal SubTotal
+        {
+            get
+            {
+                return Itens.Sum(item => item.Total);
+            }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                return SubTotal - Desconto;
+            }
+        }
+    }
+}
