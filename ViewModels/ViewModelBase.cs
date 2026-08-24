72628a6a1a75b6afb877fd5e58a726d292512702
User <User@DESKTOP-4T0P2PC>
2026-08-24 20:47:37 -0300

    Adicionar arquivos de projeto.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CursoAvalonia.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CursoAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
